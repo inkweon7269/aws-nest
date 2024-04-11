e18efc94b819f2927a53d8c99d73519cca1c6d0b
Inkwon Kim <inkweon7269@gmail.com>
2024-04-12 01:12:30 +0900

    AWS SES & nodemailer

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { SES, SendRawEmailCommand } from '@aws-sdk/client-ses';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'AWS 인스턴스 배포 완료!';
+  private readonly transporter: nodemailer.Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      SES: {
+        ses: new SES({
+          apiVersion: '2010-12-01',
+          region: 'ap-northeast-2',
+          credentials: {
+            accessKeyId: '', // IAM 액세스 키
+            secretAccessKey: '', // IAM 비밀 액세스 키
+          },
+        }),
+        aws: { SendRawEmailCommand },
+      },
+    });
+  }
+
+  async sendMailForm() {
+    const response = await this.transporter.sendMail({
+      from: '', // AWS SES에서 인증받은 메일
+      to: '', // 받는 사람 이메일
+      subject: 'Nice to meet you~!!', // 메일 제목
+      html: `<h1>Hello World</h1>`, // 본문 내용
+    });
+    return response;
   }
 }
