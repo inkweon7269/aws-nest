b04f32f7d5288c2db22659d1229f26d34dadc935
Inkwon Kim <inkweon7269@gmail.com>
2024-04-20 16:19:42 +0900

    환경변수 추가

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,15 +3,21 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export function TypeormConfig(configService: ConfigService) {
   const option: TypeOrmModuleOptions = {
-    type: '', // DB 종류
-    host: '', // HOST 정보
-    port: 5432, // PORT 정보
-    username: '', // DB 아이디
-    password: '', // DB 비밀번호
-    database: '', // 데이터베이스명
+    type: 'postgres', // DB 종류
+    host: configService.get(`DB_HOST`), // HOST 정보
+    port: +configService.get<number>(`DB_PORT`), // PORT 정보
+    username: configService.get(`DB_USERNAME`), // DB 아이디
+    password: configService.get(`DB_PASSWORD`), // DB 비밀번호
+    database: configService.get(`DB_DATABASE`), // 데이터베이스명
     autoLoadEntities: true,
     synchronize: true,
     useUTC: false,
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   };
 
   return option;
